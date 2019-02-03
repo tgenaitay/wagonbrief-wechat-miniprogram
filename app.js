07f184c1960a0ef6b64866cc69a4b247525e74e8
thibault genaitay <tgenaitay@gmail.com>
2019-02-03 19:44:01 +0800

    cleaning userinfo

--- app.js
+++ app.js
@@ -13,24 +13,6 @@ AV.init({
 App({
   data: {
   },
-  getUserInfo: function (cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
   onLaunch: function() {
     console.log("the app is launched")
   },
